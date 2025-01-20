457b1e4251350e117ed626a1bd8d00a7cd8c111a
14 <14>
2025-01-20 16:23:44 +0800

    1

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -4,7 +4,7 @@ export const zhNavbar = navbar([
   {
     text: "商城",
     icon: 'shop',
-    link: "https://mall.likeyou168.cn",
+    link: "https://shop126239208.taobao.com/?spm=pc_detail.29232929/evo401271b517998.shop_block.dshopinfo.5e8f7dd6cDA2U1",
   },
   {
     text: "社区",
